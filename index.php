bcb731406e7411a47b636dd539a3812f5d36d216
Herick <herickcwilke@gmail.com>
2019-09-03 20:13:51 -0300

    Aplicação acessando no localhost/api2 via XAMPP e aparecendo nada aqui, também se acessar /users retorna not allowed

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+
+$f3 = Base::instance();
+
+$f3->set('DEBUG', 3);
+$f3->route('POST /users', function( $f3, $params) {
+	print_r($f3 );
+
+});
+
+$f3->route('GET /', function($f3 ){
+	echo 'Nada aqui.';
+
+});
+
+$f3->run();
